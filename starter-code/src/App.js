40152233e68004194bdbaa142c908e8c3bbcef7a
Marcel <marcel.riek@googlemail.com>
2020-03-17 19:00:55 +0100

    added App.js and App.css

---
+++ starter-code/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Say Hello to ReactJS </h1>
+        <p>
+          You will learn a frontend framework from scratch to become a
+          developer.
+        </p>
+        <div>
+          <button>Awesome!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
